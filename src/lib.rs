765a694b72db06e5b3724622a3cda25e0d056a49
John Alberse <alberse.john@gmail.com>
2022-09-11 12:22:11 -0500

    Remove duplicate const conversion mat
    Removes a duplicate of OPENGL_TO_WGPU_MATRIX which has been
    moved to the camera module.

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,6 @@ use winit::{
     window::WindowBuilder,
 };
 
-#[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.0,
-    0.0, 0.0, 0.5, 1.0,
-);
-
 #[repr(C)]
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 struct LightUniform {
