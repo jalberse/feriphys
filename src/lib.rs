29a044618f0582138a75ab510a2b9a42023c2415
John Alberse <alberse.john@gmail.com>
2022-09-11 12:21:32 -0500

    Change instance to just one at center
    We don't need to show off our instancing anymore.

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,6 @@ use winit::{
     window::WindowBuilder,
 };
 
-// Temporary constants for use as we develop instancing.
-const NUM_INSTANCES_PER_ROW: u32 = 10;
-
 #[rustfmt::skip]
 pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     1.0, 0.0, 0.0, 0.0,
@@ -227,28 +224,17 @@ impl State {
                 label: Some("texture_bind_group_layout"),
             });
 
-        const SPACE_BETWEEN: f32 = 3.0;
-        let instances = (0..NUM_INSTANCES_PER_ROW)
-            .flat_map(|z| {
-                (0..NUM_INSTANCES_PER_ROW).map(move |x| {
-                    let x = SPACE_BETWEEN * (x as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-                    let z = SPACE_BETWEEN * (z as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-
-                    let position = cgmath::Vector3 { x, y: 0.0, z };
-
-                    let rotation = if position.is_zero() {
-                        cgmath::Quaternion::from_axis_angle(
-                            cgmath::Vector3::unit_z(),
-                            cgmath::Deg(0.0),
-                        )
-                    } else {
-                        cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(45.0))
-                    };
-
-                    Instance { position, rotation }
-                })
-            })
-            .collect::<Vec<_>>();
+        let origin = cgmath::Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let no_rotation =
+            cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0));
+        let instances = vec![Instance {
+            position: origin,
+            rotation: no_rotation,
+        }];
 
         // Reduce instance information to a single matrix for placement in buffer
         let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
