de736dcd3dbff5c5cd4f4929677ac8da3aa6199d
John Alberse <alberse.john@gmail.com>
2022-10-26 21:04:59 -0500

    Add RK4 integration to State
    Adds runge kutta 4th order integration to the State struct. This will
    allow us to numerically integrate using RK4 for arbitrary stateful
    types.

--- src/simulation/state.rs
+++ src/simulation/state.rs
@@ -1,4 +1,5 @@
-use itertools::Itertools;
+use crate::utils;
+use itertools::{izip, Itertools};
 
 pub trait Stateful {
     /// Number of f32 elements that are used to represent this object in the State vector.
@@ -74,6 +75,29 @@ impl<T: Stateful> State<T> {
             .collect_vec();
         let new_state_vector = crate::utils::vec_add(&self.as_vector(), &state_delta);
         State::from_state_vector(new_state_vector)
+    }
+
+    /// Performs one step of runge kutta fourth order integration, returning the next state.
+    pub fn rk4_step(&self, timestep: f32) -> State<T> {
+        let k1 = self.derivative();
+        let half_k1_delta = utils::scale(&k1, timestep * 0.5);
+        let k2 = State::<T>::from_state_vector(utils::vec_add(&self.as_vector(), &half_k1_delta))
+            .derivative();
+        let half_k2_delta = utils::scale(&k2, timestep * 0.5);
+        let k3 = State::<T>::from_state_vector(utils::vec_add(&self.as_vector(), &half_k2_delta))
+            .derivative();
+        let k3_delta = utils::scale(&k3, timestep);
+        let k4 = State::<T>::from_state_vector(utils::vec_add(&self.as_vector(), &k3_delta))
+            .derivative();
+        let delta = izip!(k1, k2, k3, k4)
+            .map(|(k1i, k2i, k3i, k4i)| {
+                timestep / 6.0 * k1i
+                    + timestep / 3.0 * k2i
+                    + timestep / 3.0 * k3i
+                    + timestep / 6.0 * k4i
+            })
+            .collect_vec();
+        State::from_state_vector(utils::vec_add(&self.as_vector(), &delta))
     }
 
     /// Drops self, returning the State as a Vec<T>.
@@ -177,5 +201,100 @@ mod tests {
         };
         assert_eq!(expected_position, new_point.position);
         assert_eq!(expected_velocity, new_point.velocity);
+    }
+
+    struct ExampleFn {
+        y: f32,
+        t: f32,
+        timestep: f32,
+    }
+
+    impl Stateful for ExampleFn {
+        fn num_state_elements() -> usize {
+            3
+        }
+
+        fn from_state_vector(state_data: Vec<f32>) -> Self {
+            if state_data.len() != Self::num_state_elements() {
+                panic!("State Vector incorrect size!")
+            }
+            ExampleFn {
+                y: state_data[0],
+                t: state_data[1],
+                timestep: state_data[2],
+            }
+        }
+
+        fn derivative(&self) -> Vec<f32> {
+            vec![self.y - f32::powi(self.t, 2) + 1.0, 1.0, 0.0]
+        }
+
+        fn as_state(&self) -> Vec<f32> {
+            vec![self.y, self.t, self.timestep]
+        }
+    }
+
+    #[test]
+    fn rk4_step() {
+        let h = 0.5;
+        let ex = vec![ExampleFn {
+            y: 0.5,
+            t: 0.0,
+            timestep: h,
+        }];
+        let state = super::State::new(ex);
+        let expected_initial_state = vec![0.5, 0.0, 0.5];
+        assert_eq!(expected_initial_state, state.as_vector());
+
+        let acceptable_error = 0.005;
+
+        // The exact solution is y = t^2 + 2t + 1 - .5e^t
+
+        // Take the first step, t = 0.5
+        let state = state.rk4_step(h);
+        let new_state_vec = state.get_elements();
+        let new_state_ex = &new_state_vec[0];
+        assert!(
+            1.425130208333333 + acceptable_error > new_state_ex.y
+                && 1.425130208333333 - acceptable_error < new_state_ex.y
+        );
+        assert_eq!(0.5, new_state_ex.t);
+        assert_eq!(0.5, new_state_ex.timestep);
+
+        // Take the second step
+        let state = super::State::new(new_state_vec);
+        let state = state.rk4_step(h);
+        let new_state_vec = state.get_elements();
+        let new_state_ex = &new_state_vec[0];
+        assert!(
+            2.640859085770477 + acceptable_error > new_state_ex.y
+                && 2.640859085770477 - acceptable_error < new_state_ex.y
+        );
+        assert_eq!(1.0, new_state_ex.t);
+        assert_eq!(0.5, new_state_ex.timestep);
+
+        // Third step
+        let state = super::State::new(new_state_vec);
+        let state = state.rk4_step(h);
+        let new_state_vec = state.get_elements();
+        let new_state_ex = &new_state_vec[0];
+        assert!(
+            4.009155464830968 + acceptable_error > new_state_ex.y
+                && 4.009155464830968 - acceptable_error < new_state_ex.y
+        );
+        assert_eq!(1.5, new_state_ex.t);
+        assert_eq!(0.5, new_state_ex.timestep);
+
+        // Fourth step
+        let state = super::State::new(new_state_vec);
+        let state = state.rk4_step(h);
+        let new_state_vec = state.get_elements();
+        let new_state_ex = &new_state_vec[0];
+        assert!(
+            5.305471950534675 + acceptable_error > new_state_ex.y
+                && 5.305471950534675 - acceptable_error < new_state_ex.y
+        );
+        assert_eq!(2.0, new_state_ex.t);
+        assert_eq!(0.5, new_state_ex.timestep);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use std::ops::Mul;
+
+use itertools::Itertools;
+
 pub fn vec_add<T>(v1: &[T], v2: &[T]) -> Vec<T>
 where
     T: std::ops::Add<Output = T> + Copy,
@@ -7,4 +11,11 @@ where
     }
 
     v1.iter().zip(v2).map(|(&i1, &i2)| i1 + i2).collect()
+}
+
+pub fn scale<T>(vec: &[T], scalar: f32) -> Vec<T>
+where
+    T: Mul<f32, Output = T> + Copy,
+{
+    vec.into_iter().map(|x| *x * scalar).collect_vec()
 }
