ade27263f8dd6e54f786749953b6c6c58cfb9b1d
John Alberse <alberse.john@gmail.com>
2022-10-31 21:35:11 -0500

    Adjust params to make cube stiffer

--- src/simulation/springy/springy_mesh.rs
+++ src/simulation/springy/springy_mesh.rs
@@ -8,8 +8,8 @@ use itertools::Itertools;
 use rustc_hash::FxHashMap;
 
 // TODO adjust these (or if these are "okay", we can use them and make them adjustable in UI)
-pub const STRUT_STIFFNESS_DEFAULT: f32 = 10.0;
-pub const STRUT_DAMPING_DEFAULT: f32 = 3.0;
+pub const STRUT_STIFFNESS_DEFAULT: f32 = 100.0;
+pub const STRUT_DAMPING_DEFAULT: f32 = 100.0;
 // Generally, torsional spring and damping parameters should be one or two orders of magnitude higher
 // than the corresponding strut parameters.
 pub const TORSIONAL_SPRING_STIFFNESS_DEFAULT: f32 = 100.0;
